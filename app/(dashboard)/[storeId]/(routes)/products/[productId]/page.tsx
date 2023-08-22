52a09a3f27746209488e7283fa7fa2f90cb7db82
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-08-23 03:12:25 +0700

    test add rsc

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -1,54 +1,62 @@
 "use server";
 
-import prismadb from "@/lib/prismadb";
+import prismadb from "@/lib/prismadb"
 
-
-import { ProductForm } from "./components/product-form";
+import { ProductForm } from "./components/product-form"
 
 const ProductPage = async ({
-  params
+	params,
 }: {
-  params: { productId: string, storeId: string }
+	params: { productId: string; storeId: string };
 }) => {
-  const product = await prismadb.product.findUnique({
-    where: {
-      id: params.productId,
-    },
-    include: {
-      images: true,
-    }
-  });
+	const product = await prismadb.product.findUnique({
+		where: {
+			id: params.productId,
+		},
+		include: {
+			images: true,
+		},
+	});
+	if (product) {
+		const sold = await prismadb.orderItem.count({
+			where: {
+				productId: product.id,
+			},
+		});
 
-  const categories = await prismadb.category.findMany({
-    where: {
-      storeId: params.storeId,
-    },
-  });
+		product.stock = product.stock - sold;
+	}
 
-  const sizes = await prismadb.size.findMany({
-    where: {
-      storeId: params.storeId,
-    },
-  });
+	const categories = await prismadb.category.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
 
-  const colors = await prismadb.color.findMany({
-    where: {
-      storeId: params.storeId,
-    },
-  });
+	const sizes = await prismadb.size.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
 
-  return ( 
-    <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductForm 
-          categories={categories} 
-          colors={colors}
-          sizes={sizes}
-          initialData={product}
-        />
-      </div>
-    </div>
-  );
-}
+	const colors = await prismadb.color.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+	});
+
+	return (
+		<div className="flex-col">
+			<div className="flex-1 space-y-4 p-8 pt-6">
+				<ProductForm
+					categories={categories}
+					colors={colors}
+					sizes={sizes}
+					initialData={product}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default ProductPage;
