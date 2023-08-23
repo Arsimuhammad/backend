bdec0b1671b90aceb295703349f18a85ca99a04e
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-08-23 13:13:58 +0700

    test add rsc 2

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,23 +1,148 @@
 "use client";
 
-import { DataTable } from "@/components/ui/data-table";
-import { Heading } from "@/components/ui/heading";
-import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
 
-import { columns, OrderColumn } from "./columns";
+import { Button } from "@/components/ui/button"
+import jsPDF from 'jspdf'
+import 'jspdf-autotable'
+import { useParams, useRouter } from "next/navigation"
+import { useState } from "react"
+import { columns, OrderColumn } from "./columns"
 
 interface OrderClientProps {
-  data: OrderColumn[];
+	data: OrderColumn[];
 }
 
-export const OrderClient: React.FC<OrderClientProps> = ({
-  data
-}) => {
-  return (
-    <>
-      <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
-      <Separator />
-      <DataTable searchKey="products" columns={columns} data={data} />
-    </>
-  );
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+	const router = useRouter();
+	const params = useParams();
+
+	const [filterDateByMonth, setFilterDateByMonth] = useState<Date | null>(null);
+	const [filterDateByDate, setFilterDateByDate] = useState<Date | null>(null);
+
+	const handleFilterChange = (date: Date | null) => {
+		setFilterDateByMonth(date);
+	};
+
+	const generatePDF = () => {
+		const doc = new jsPDF();
+		// @ts-ignore
+		doc.autoTable({
+			head: [columns.map((column) => column.header)],
+			body: data.map((item) => [
+				item.products,
+				item.name,
+				item.phone,
+				item.address,
+				item.totalPrice,
+				item.isPaid,
+				item.createdAt,
+			]),
+		});
+
+		doc.save("table.pdf");
+	};
+
+	return (
+		<>
+			<Heading
+				title={`Orders (${data.length})`}
+				description="Manage orders for your store"
+			/>
+			<div className="flex justify-between gap-2">
+				<div className="grid grid-cols-2 gap-2">
+					<div className="flex gap-2 items-center">
+						<div>
+							<label htmlFor="filterMonth">Filter by Month: </label>
+							<input
+								type="month"
+								id="filterDateByMonth"
+								value={
+									filterDateByMonth
+										? `${filterDateByMonth.getFullYear()}-${(
+												filterDateByMonth.getMonth() + 1
+										  )
+												.toString()
+												.padStart(2, "0")}`
+										: ""
+								}
+								onChange={(e) => handleFilterChange(new Date(e.target.value))}
+							/>
+						</div>
+						<Button
+							variant="default"
+							size="sm"
+							onClick={() =>
+								router.push(
+									`/${params.storeId}/orders?month=${
+										filterDateByMonth
+											? `${filterDateByMonth.getFullYear()}-${(
+													filterDateByMonth.getMonth() + 1
+											  )
+													.toString()
+													.padStart(2, "0")}`
+											: ""
+									}`
+								)
+							}
+						>
+							Filter
+						</Button>
+					</div>
+					<div className="flex gap-2 items-center">
+						<div>
+							<label htmlFor="filterDate">Filter by Date: </label>
+							<input
+								type="date"
+								id="filterDateByDate"
+								value={
+									filterDateByDate
+										? `${filterDateByDate.getFullYear()}-${(
+												filterDateByDate.getMonth() + 1
+										  )
+												.toString()
+												.padStart(2, "0")}-${filterDateByDate
+												.getDate()
+												.toString()
+												.padStart(2, "0")}`
+										: ""
+								}
+								onChange={(e) => setFilterDateByDate(new Date(e.target.value))}
+							/>
+						</div>
+						<Button
+							variant="default"
+							size="sm"
+							onClick={() =>
+								router.push(
+									`/${params.storeId}/orders?date=${
+										filterDateByDate
+											? `${filterDateByDate.getFullYear()}-${(
+													filterDateByDate.getMonth() + 1
+											  )
+													.toString()
+													.padStart(2, "0")}-${filterDateByDate
+													.getDate()
+													.toString()
+													.padStart(2, "0")}`
+											: ""
+									}`
+								)
+							}
+						>
+							Filter
+						</Button>
+					</div>
+				</div>
+
+				<Button variant="default" size="sm" onClick={() => generatePDF()}>
+					Export
+				</Button>
+			</div>
+			<Separator />
+			<DataTable searchKey="products" columns={columns} data={data} />
+		</>
+	);
 };
--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -6,54 +6,51 @@ import { formatter } from "@/lib/utils"
 import { ProductsClient } from "./components/client"
 import { ProductColumn } from "./components/columns"
 
-const ProductsPage = async ({
-  params
-}: {
-  params: { storeId: string }
-}) => {
-  const products = await prismadb.product.findMany({
-    where: {
-      storeId: params.storeId
-    },
-    include: {
-      category: true,
-      size: true,
-      color: true,
-    },
-    orderBy: {
-      createdAt: 'desc'
-    }
-  });
+const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
+	const products = await prismadb.product.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+		include: {
+			category: true,
+			size: true,
+			color: true,
+		},
+		orderBy: {
+			createdAt: "desc",
+		},
+	});
 
-  const formattedProducts: ProductColumn[] = await Promise.all(products.map(async (item) => {
-const sold = await prismadb.orderItem.count({
-        where: {
-          productId: item.id,
-        }
-      })
+	const formattedProducts: ProductColumn[] = await Promise.all(
+		products.map(async (item) => {
+			const sold = await prismadb.orderItem.count({
+				where: {
+					productId: item.id,
+				},
+			});
 
-      item.stock = item.stock - sold
-    return {
-    id: item.id,
-    name: item.name,
-    isFeatured: item.isFeatured,
-    isArchived: item.isArchived,
-    price: formatter.format(item.price.toNumber()),
-          stock: item.stock,
-    category: item.category.name,
-    size: item.size.name,
-    color: item.color.value,
-    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
-  }
-  }))
+			return {
+				id: item.id,
+				name: item.name,
+				isFeatured: item.isFeatured,
+				isArchived: item.isArchived,
+				price: formatter.format(item.price.toNumber()),
+				stock: +item.stock - +sold,
+				category: item.category.name,
+				size: item.size.name,
+				color: item.color.value,
+				createdAt: format(item.createdAt, "MMMM do, yyyy"),
+			};
+		})
+	);
 
-  return (
-    <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductsClient data={formattedProducts} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex-col">
+			<div className="flex-1 space-y-4 p-8 pt-6">
+				<ProductsClient data={formattedProducts} />
+			</div>
+		</div>
+	);
 };
 
 export default ProductsPage;
